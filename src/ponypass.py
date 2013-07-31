ffa84e2a13cc65f452408c5288f2c45ebda60b38
Mattias Andrée <maandree@operamail.com>
2013-07-31 17:09:37 +0200

    m + create entry
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/ponypass.py
+++ src/ponypass.py
@@ -22,6 +22,9 @@ import os
 import sys
 from subprocess import Popen, PIPE
 
+import correctpony
+import obscurepass
+
 
 HOME = os.environ['HOME']
 WALLET_DIR = '%s/.ponypass'
@@ -32,7 +35,7 @@ GPG_ID = '%s/id' % WALLET_DIR
 def gpg_encrypt(data, file):
     id = None
     if not os.path.exists(GPG_ID):
-        print('\e[01;34mPlease enter your GPG key ID.\e[00m')
+        print('\033[01;34mPlease enter your GPG key ID.\033[00m')
         id = input()
         with open(GPG_ID, 'wb') as idfile:
             idfile.write((id + '\n').encode('utf-8'))
@@ -50,7 +53,7 @@ def gpg_encrypt(data, file):
         gpg = Popen(['gpg', '--encrypt', '--recipient', id], stdin = PIPE, stdout = PIPE)
         out = gpg.communicate(data)[0]
         if gpg.returncode != 0:
-            print('\e[01;34mGPG failed, press enter to retry or C-c to abort.\e[00m')
+            print('\033[01;34mGPG failed, press enter to retry or C-c to abort.\033[00m')
         else:
             with open(file, 'wb') as wfile:
                 wfile.write(out)
@@ -65,25 +68,97 @@ def gpg_decrypt(file):
         gpg = Popen(['gpg', '--decrypt'], stdin = PIPE, stdout = PIPE)
         out = gpg.communicate(data)[0]
         if gpg.returncode != 0:
-            print('\e[01;34mGPG failed, press enter to retry or C-c to abort.\e[00m')
+            print('\033[01;34mGPG failed, press enter to retry or C-c to abort.\033[00m')
         else:
             return out.decode('utf-8', 'error')
 
 
 if not os.path.exists(WALLET):
     os.makedirs(WALLET_DIR, exist_ok = True)
-    print('\e[01;34mCreating a wallet.\e[00m', file = sys.stderr)
+    print('\033[01;34mCreating a wallet.\033[00m', file = sys.stderr)
     gpg_encrypt('()\n'.encode('utf-8'), WALLET)
 elif not os.path.isfile(WALLET):
     print('%s: %s is not a regular file or link to a regular file' % (sys.argv[0], WALLET), file = sys.stderr)
     sys.exit(1)
 
 
-print('\e[01;34mLoading wallet.\e[00m', file = sys.stderr)
-wallet = gpg_decrypt(WALLET)
+print('\033[01;34mLoading wallet.\033[00m', file = sys.stderr)
+wallet = eval(gpg_decrypt(WALLET))
+passphrase = None
+user = None
+email = None
 
-# TODO
 
-print('\e[01;34mSaving wallet.\e[00m', file = sys.stderr)
-gpg_crypt(wallet, WALLET)
+if len(sys.argv) == 1:
+    pass ## TODO
+
+elif sys.argv[1] == 'edit':
+    pass ## TODO
+
+elif sys.argv[1] == 'delete':
+    pass ## TODO
+
+elif sys.argv[1] == 'add':
+    print('\033[01;34mSite:\033[00m  ', end = '')
+    sys.stdout.flush()
+    site = input()
+    print('\033[01;34mUsername:\033[00m  ', end = '')
+    sys.stdout.flush()
+    user = input()
+    print('\033[01;34mE-mail:\033[00m  ', end = '')
+    sys.stdout.flush()
+    email = input()
+    passwordmenu = [{'title' : 'Create random passphrase', 'description' : 'Using correctpony', 'value' : 0},
+                    {'title' : 'Create random obscure password', 'description' : 'Not typeable nor fully visible', 'value' : 1},
+                    {'title' : 'Enter passphrase manually', 'description' : 'Correctpony will give a suggestion', 'value' : 2}]
+    passwordoption = pass ## TODO
+    if passwordoption = 0:
+        passphrase = correctpony.gensimple()
+    elif passwordoption = 1:
+        passphrase = obscurepass.genobsure()
+    else:
+        saved_tty = Popen(['stty', '--save'], stdout=PIPE, stderr=sys.stderr).communicate()[0].decode('utf-8', 'error')[:-1]
+        try:
+            Popen(['stty', '-echo'], stdin=sys.stdout).wait()
+            print('\033[01;34mSuggested passphrase:\033[00m %s' % correctpony.gensimple())
+            while True:
+                print('\033[01;34mPassphrase:\033[00m  ', end = '')
+                sys.stdout.flush()
+                passphrase = input()
+                print('\033[01;34mRetype passphrase:\033[00m  ', end = '')
+                sys.stdout.flush()
+                passphrase_re = input()
+                if passphrase_re == passphrase:
+                    break
+                print('\033[01;31mPassphrases did not match eachother!\033[00m')
+        finally:
+            Popen(['stty', saved_tty], stdin=sys.stdout).wait()
+    append_to = wallet
+    for entry in wallet:
+        if entry['title'] == site:
+            if 'inner' in entry:
+                append_to = entry['inner']
+            else:
+                inner = [entry['value']]
+                del entry['value']
+                entry['inner'] = inner
+                append_to = entry['inner']
+            break
+    append_to.append({'title' : site if append_to is wallet else user,
+                      'user' : user, 'pass' : passphrase, 'email' : email})
+    append_to.sort(key = lambda item : item['title'])
+
+
+print('\033[01;34mSaving wallet.\033[00m', file = sys.stderr)
+gpg_crypt(str(wallet), WALLET)
+
+if user is not None:
+    print('\033[01;34mYour username:\033[00m')
+    print(user)
+if email is not None:
+    print('\033[01;34mYour e-mail:\033[00m')
+    print(email)
+if passphrase is not None:
+    print('\033[01;34mYour passphrase in invisible text:\033[00m')
+    print('\033[30;40m%s\033[00;01;33;41mEND\033[00m' % passphrase)
 
